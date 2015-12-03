5596ef80013cff1b7dff33e9417d2e9b4f1eeee7
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2015-12-03 16:37:15 -0500

    add bounds for gravity

--- TheMostAwesome/GameViewController.swift
+++ TheMostAwesome/GameViewController.swift
@@ -24,6 +24,7 @@ class GameViewController: UIViewController {
             skView.ignoresSiblingOrder = true
             
             /* Set the scale mode to scale to fit the window */
+            scene.size = skView.bounds.size
             scene.scaleMode = .AspectFill
             
             skView.presentScene(scene)
