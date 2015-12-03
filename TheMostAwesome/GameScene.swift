f22aae340361a1dbbdfd05519bc63662adaf6ce1
Bruce Demske <bdemske@ella.net>
2015-12-03 15:32:01 -0500

    Added the golf ball, commented the other ball

--- TheMostAwesome/GameScene.swift
+++ TheMostAwesome/GameScene.swift
@@ -16,29 +16,35 @@ class GameScene: SKScene {
         myLabel.fontSize = 45;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        let ballNode = SKSpriteNode.init(color: UIColor.blueColor(), size: CGSize(width: 10,height: 10))
+        /*let ballNode = SKSpriteNode.init(color: UIColor.blueColor(), size: CGSize(width: 10,height: 10))
         ballNode.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(ballNode)
+        self.addChild(ballNode)*/
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            for touch in touches {
+                let location = touch.locationInNode(self)
+                
+                let sprite = SKSpriteNode(imageNamed:"Golfball")
+                
+                sprite.xScale = 1
+                sprite.yScale = 1
+                sprite.position = location
+                
+                let action1 = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+                
+                sprite.runAction(SKAction.repeatActionForever(action1))
+                
+                let vector = CGVectorMake(25.0, 25.0)
+                
+                let action2 = SKAction.moveBy(vector, duration: 2.0)
+                
+                sprite.runAction(SKAction.repeatActionForever(action2))
+                
+                self.addChild(sprite)
         }
     }
    
