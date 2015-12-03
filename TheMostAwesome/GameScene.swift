202fd54d60f72995b85ebeb46e9b8ae385a636d1
Cassie Sandquist <cassandra.g.sandquist@lowes.com>
2015-12-03 16:10:59 -0500

    add world gravity

--- TheMostAwesome/GameScene.swift
+++ TheMostAwesome/GameScene.swift
@@ -16,6 +16,13 @@ class GameScene: SKScene {
         myLabel.fontSize = 45;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -9.81)
+        
+        let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        borderBody.friction = 0.0
+        self.physicsBody = borderBody
+        
+        
         /*let ballNode = SKSpriteNode.init(color: UIColor.blueColor(), size: CGSize(width: 10,height: 10))
         ballNode.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
@@ -32,20 +39,30 @@ class GameScene: SKScene {
                 
                 sprite.xScale = 1
                 sprite.yScale = 1
-                sprite.position = location
-                
-                let action1 = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-                
-                sprite.runAction(SKAction.repeatActionForever(action1))
-                
-                let vector = CGVectorMake(25.0, 25.0)
-                
-                let action2 = SKAction.moveBy(vector, duration: 2.0)
-                
-                sprite.runAction(SKAction.repeatActionForever(action2))
+                sprite.name = "ball"
+                sprite.position = location //CGPointMake(self.frame.size.width/3, self.frame.size.height/3)
+                sprite.physicsBody = SKPhysicsBody(circleOfRadius: sprite.frame.size.width/2)
+                sprite.physicsBody?.friction = 0
+                sprite.physicsBody?.restitution = 1.0
+                sprite.physicsBody?.linearDamping = 0
+                sprite.physicsBody?.allowsRotation = true
+                sprite.physicsBody?.applyImpulse(CGVector(dx: 10.0, dy: -10.0))
                 
                 self.addChild(sprite)
+                
+                addAnAction(sprite)
+                addAction2(sprite)
         }
+    }
+    
+    func addAnAction(sprite: SKSpriteNode){
+        let action1 = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        sprite.runAction(SKAction.repeatActionForever(action1))
+    }
+    func addAction2(sprite:SKSpriteNode){
+        let vector = CGVectorMake(25.0, 25.0)
+        let action2 = SKAction.moveBy(vector, duration: 2.0)
+        sprite.runAction(SKAction.repeatActionForever(action2))
     }
    
     override func update(currentTime: CFTimeInterval) {
